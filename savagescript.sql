da5ea96ad42cbc9cdf654da6873656c40aafa0b2
securitysavage <86885086+securitysavage@users.noreply.github.com>
2021-07-11 10:55:13 -0700

    Update savagescript.sql
    Forgot to tell docker-compose to use the local Dockerfile, which broke everything lol rekt

--- savagescript.sql
+++ savagescript.sql
@@ -1,4 +1,5 @@
 -- Change DB name and user to your docker-compose.yml values
+CREATE USER savage@'%' IDENTIFIED BY 'savagewiki';
 CREATE DATABASE savagewiki;
 GRANT ALL PRIVILEGES ON savagewiki.* TO savage@'%';
 flush privileges;
